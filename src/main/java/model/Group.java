1a4c4b9a070c4fe6c2e611f48321585118462e62
JamesCooper97 <james.cooper97@outlook.com>
2018-09-04 17:41:56 +0100

    04092018

---
+++ src/main/java/model/Group.java
@@ -0,0 +1,31 @@
+package model;
+
+public class Group {
+	private String groupId;
+	private String groupName;
+	private int groupSize;
+	
+	public String getGroupId() {
+		return groupId;
+	}
+	
+	public void setGroupId(String groupId) {
+		this.groupId = groupId;
+	}
+	
+	public String getGroupName() {
+		return groupName;
+	}
+	
+	public void setGroupName(String groupName) {
+		this.groupName = groupName;
+	}
+	
+	public int getGroupSize() {
+		return groupSize;
+	}
+	
+	public void setGroupSize(int groupSize) {
+		this.groupSize = groupSize;
+	}
+}
---
+++ src/main/java/model/Sport.java
@@ -0,0 +1,8 @@
+package model;
+
+public class Sport {
+	private String sportId;
+	private String sportName;
+	private int teamSize;
+
+}
